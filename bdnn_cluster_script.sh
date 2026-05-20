b6a1a6534b55b2250f68bb7418da48ed2523e0ae
S W <sw@mac.mynetworksettings.com>
2026-05-20 02:50:59 -0400

    made bdnn run properly: environ data needed to be .txt format, fixed cluster script, removed some files unneeded

--- bdnn_cluster_script.sh
+++ bdnn_cluster_script.sh
@@ -22,6 +22,6 @@ module load anaconda3/2024.2
 python ../PyRate/PyRate.py input_data/pbdb_data_PyRate.py \
 -qShift Time_bins_ByStages.txt -mG \
 -BDNNmodel 1 \
--BDNNtimevar predictor_data/environmental_data_standardized.csv \
--BDNNtimetrait 0 -BDNN_nsim_expected_cv 0 -translate 199 -BDNNnodes 4 2 -BDNNupdate_f 0.3 \
+-BDNNtimevar predictor_data/environmental_data_standardized.txt \
+-BDNNtimetrait 0 -BDNN_nsim_expected_cv 0 -translate -199 -BDNNnodes 4 2 -BDNNupdate_f 0.3 \
 -n 100000 -s 100 -wd bdnn -j ${SLURM_ARRAY_TASK_ID} 
